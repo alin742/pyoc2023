a2a474ebd3b10421d43d217acdd579f89218a9c9
Ali Elnwegy <alinwegy@gmail.com>
2023-12-01 00:43:24 +0100

    ready for day 1

---
+++ template.py
@@ -0,0 +1,28 @@
+import numpy as np
+INPUT = "input.txt"
+SAMPLE = "sample.txt"
+SAMPLE_PART1 = 0 # Add Results of part1 sample
+SAMPLE_PART2 = 0 # Add Results of part2 sample
+
+
+def part1(filename):
+    # ADD PART1 HERE
+    return 0
+
+def part2(filename):
+    # ADD PART2 HERE
+    return 0
+
+def test_part1():
+    assert part1(SAMPLE) == SAMPLE_PART1
+
+def test_part2():
+    assert part2(SAMPLE) == SAMPLE_PART2
+
+def main():
+    print(f"part 1: {part1(INPUT)}")
+    print(f"part 2: {part2(INPUT)}")
+
+
+if __name__ == "__main__":
+    main()
